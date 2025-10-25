6639168b45421e919f981202a3001bc4d6bdd32c
TheOriginalBytePlayer <prez@frameforge3d.com>
2025-10-25 04:06:14 -0700

    Update Build wrapper.bat

--- wrapper/Build wrapper.bat
+++ wrapper/Build wrapper.bat
@@ -1,5 +1,5 @@
 REM mkdir build
 cd build
-REM cmake .. -DBUILD_SHARED_LIBS=ON -Dozz_build_samples=OFF -Dozz_build_howtos=OFF -Dozz_build_tests=OFF
+cmake .. -DBUILD_SHARED_LIBS=ON -Dozz_build_samples=OFF -Dozz_build_howtos=OFF -Dozz_build_tests=OFF
 make cal3d_wrapper
 pause
